f3b85aab92f51415135c6acccc2fcc4656119b2f
ReinerYaeger <chevking101@gmail.com>
2021-10-18 10:37:33 +0000

    Fixed create credit card

--- ServiceProvider.h
+++ ServiceProvider.h
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-class ServiceProvider{
+class ServiceProvider: public exception{
 
 private:
 	string companyId;
@@ -66,19 +66,49 @@ public:
 
 	void createPhoneCard(){
 		string buffer;
-		int hold;
+		int hold,choice;
+		const int demoniation[4]={100,200,500,1000};
+		
+		try{
+			cout << "Create Phone Card\n\n" <<
+						"Select Denomination:\n" <<
+						"1 for $100\n" <<
+						"2 for $200\n" <<
+						"3 for $500\n" <<
+						"4 for $1000\n" << endl;
+			cin >> choice;
+			if(cin.fail())
+				throw runtime_error("Value not Expected");
+			if(choice != 1 || choice != 2 ||  choice != 3 || choice != 4 )
+				throw -1;
+		}catch(runtime_error &err){
+			cerr << err.what() << endl;
+		}catch(int &err){
+			cerr << "Enter one of the values displayed\n" << endl;
+		}catch(...){
+			cerr << "A fatal error has occurred "<< endl;
+			return;
+		}
+		int sentenel;
+
+		if(choice == 4)
+			sentenel=8;	
+		else
+			sentenel = 9;
+		
 		random_device rand; //Getting a random number
 		mt19937 generate(rand()); //Random number generator 
 		uniform_int_distribution<> distribution(0,9); //random number range definition
 
-		for(int i=0;i <=12;i++){
+		for(int i=0;i <=sentenel;i++){
 			hold = (distribution(generate)); //assigning random number to hold
 			buffer += to_string(hold); //type casting hold to string and storing it in buffer
 		}
 
+		buffer += to_string(demoniation[choice-1]); //casting the value of each credit to the end of the card number;
 		cout <<"\n\n" << endl;
 		this->creditNumber = buffer;
-			cout << "New Phone Card is: " << this->creditNumber << "\n\n" <<endl;
+		cout << "New Phone Card is: " << this->creditNumber << "\n\n" <<endl;
 	}
 
 	int viewTotalNumberOfCustomer(){//Could be overridden or maybe it should how all customers
